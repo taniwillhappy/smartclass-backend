d4114e8e57436ba1d9c3ecd990a4bf4519133591
taniwillhappy <chidewch@gmail.com>
2026-02-04 22:49:43 +0700

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,16 +1,11 @@
-const express = require('express');
-const router = express.Router();
-const {
-  register,
-  login,
-  dashboard
-const authController = require('../controllers/authController');
+const express = require('express')
+const router = express.Router()
 
-const auth = require("../middleware/authMiddleware");
+const { register, login, dashboard } = require('../controllers/authController')
+const auth = require('../middleware/authMiddleware')
 
-router.get("/dashboard", auth, authController.dashboard);
+router.post('/register', register)
+router.post('/login', login)
+router.get('/dashboard', auth, dashboard)
 
-router.post('/register', register);
-router.post('/login', login);
-
-module.exports = router;
+module.exports = router
