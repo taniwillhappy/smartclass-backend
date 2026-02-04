96e70a72345981b526ca64821b0db64c30f8fd6e
taniwillhappy <chidewch@gmail.com>
2026-02-04 21:29:52 +0700

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,10 +1,9 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
+
 
 module.exports = pool;
